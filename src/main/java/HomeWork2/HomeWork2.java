965c3d90e06d9b109feffe390e7377c2ba94e22b
Nikolay Kuzyakin <mikolaych@mail.ru>
2022-04-18 01:49:10 +0200

    Homework 2

--- src/main/java/HomeWork2/HomeWork2.java
+++ src/main/java/HomeWork2/HomeWork2.java
@@ -10,9 +10,12 @@ public class HomeWork2<array> {
 //        ArraySixOnTwo();
 //        ArraySqare();
 //        ArrayMinMax();
-//        ArrayDouble();
-        ArrayMove();
+        ArrayDouble();
+//        ArrayMove();
     }
+
+    /*Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1,
+            0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;*/
 
     private static void ArrayChange() {
         int array[] = {1, 0, 1, 0, 1, 1, 1, 0, 0, 1};
@@ -27,6 +30,10 @@ public class HomeWork2<array> {
         System.out.println(Arrays.toString(array));
     }
 
+    /*Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его
+    значениями 0 3 6 9 12 15 18 21;*/
+
+
     private static void ArrayLoad() {
         int[] array = new int[8];
         int i = 0;
@@ -39,6 +46,9 @@ public class HomeWork2<array> {
         System.out.println(Arrays.toString(array));
     }
 
+   /* Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6
+    умножить на 2;*/
+
     private static void ArraySixOnTwo() {
         int array[] = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < 12; i++) {
@@ -49,6 +59,9 @@ public class HomeWork2<array> {
 
         System.out.println(Arrays.toString(array));
     }
+
+    /*Создать квадратный двумерный целочисленный массив (количество строк и столбцов
+            одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;*/
 
     private static void ArraySqare() {
         int[][] table = new int[10][10];
@@ -65,6 +78,8 @@ public class HomeWork2<array> {
         }
     }
 
+//    Задать одномерный массив и найти в нем минимальный и максимальный элементы
+
     private static void ArrayMinMax() {
         int array[] = {-7, 12, 54, 89, 1, 54, 0, 7868, 12, -89, 5, 9, 465, 7, 36, 4, 999};
         int a = 0;
@@ -80,19 +95,18 @@ public class HomeWork2<array> {
         }
         System.out.println("Наибольшее число: " + b);
         System.out.println("Наименьшее число:" + c);
-
-
     }
-    private static void ArrayDouble() {
-        int array[] = {10,8,2,3,1,1,10,2,2,1};
 
-        /*int array[] = new int [10];
+    /*Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, ||2, 1]) → true, граница показана символами ||, эти символы в массив не входя*/
+
+    private static void ArrayDouble() {
+        int array[] = new int [10];
         for (int i = 0; i < 10; i++) {
             ThreadLocalRandom random = ThreadLocalRandom.current();
             int rand = random.nextInt(0, 10);
             array[i] = rand;
         }
-        System.out.println(Arrays.toString(array));*/
+        System.out.println(Arrays.toString(array));
 
         int a = 0;
         int b = 0;
@@ -109,11 +123,13 @@ public class HomeWork2<array> {
             }
             b=0;
             }
-
-
-
-
         }
+
+    /*Написать метод, которому на вход подается одномерный массив и число n (может быть
+положительным, или отрицательным), при этом метод должен сместить все элементы массива
+    на n позиций. Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться
+    вспомогательными массивами*/
+
     private static void ArrayMove() {
         int array[] = {10,8,2,3,1,1,10,2,2,1};
         ThreadLocalRandom random = ThreadLocalRandom.current();
